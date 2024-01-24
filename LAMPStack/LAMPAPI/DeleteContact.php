dac60a7c30b65c47431fe351ba325ceba2de0a37
Wess A <114402795+DaimondSailer@users.noreply.github.com>
2024-01-24 11:54:45 -0500

    Update DeleteContact.php

--- LAMPStack/LAMPAPI/DeleteContact.php
+++ LAMPStack/LAMPAPI/DeleteContact.php
@@ -12,7 +12,7 @@
 	else
 	{
 		// $stmt = $conn->prepare("DELETE from Contacts where (ID, userID) == VALUES(?,?)");
-		$stmt = $conn->prepare("DELETE from Contacts where ID = VALUES(?)");
+		$stmt = $conn->prepare("DELETE from Contacts where ID = ?");
 		$stmt->bind_param("s", $ID);
 		$stmt->execute();
 		$stmt->close();

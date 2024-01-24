2b64409fa033df7fdfdddc372ec1c0ce38da97c6
Wess A <114402795+DaimondSailer@users.noreply.github.com>
2024-01-24 15:03:20 -0500

    added edit

---
+++ LAMPStack/LAMPAPI/EditContacts.php
@@ -0,0 +1,42 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$id = $inData["ID"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$Phone = $inData["Phone"];
+	$Email = $inData["Email"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("SELECT (?, ?, ?, ?) FROM Contacts WHERE ID=?");
+		$stmt->bind_param("ss", $firstName, $lastName, $Phone, $Email, $id);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
